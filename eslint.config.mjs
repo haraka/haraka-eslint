3bbeaa3aa131df03833ff00582a6c3a34d89b1b0
Matt Simerson <matt@tnpi.net>
2025-01-07 14:01:48 -0800

    chore: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,15 +7,18 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("eslint:recommended"), {
+export default [
+  ...compat.extends("eslint:recommended"),
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
     },
-}];
+  },
+];
